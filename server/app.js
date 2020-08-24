18ec6061b88a8d7b3063ba7a5f8b58c0367510a6
meironY2008 <67857621+meironY2008@users.noreply.github.com>
2020-08-24 16:52:41 +0300

    finish backend

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get(`/api/tickets/`, async (req, res) => {
     const ticketJson = await fs.readFile('./data.json');
     const ticketArray = JSON.parse(ticketJson);
-    const searchText = url.parse(req.url ,true).query.searchText;
+    const searchText = url.parse(req.url, true).query.searchText;
     if (searchText) {
         const filterTickectsArray = ticketArray.filter(ticket => ticket.title.toLowerCase().includes(searchText.toLowerCase()))
         res.send(filterTickectsArray);
@@ -19,5 +19,35 @@ app.get(`/api/tickets/`, async (req, res) => {
         res.send(ticketArray);
     }
 });
+
+app.post('/api/tickets/:ticketId/done', async (req, res) => {
+    let ticketJson = await fs.readFile('./data.json');
+    let ticketsArray = JSON.parse(ticketJson);
+    const doneTicketIndex = ticketsArray.findIndex(ticket => ticket.id === req.params.ticketId);
+    if (doneTicketIndex >= 0) {
+        ticketsArray[doneTicketIndex].done = true;
+        ticketJson = JSON.stringify(ticketsArray, null, 2);
+        await fs.writeFile('./data.json', ticketJson);
+        res.send(ticketsArray[doneTicketIndex]);
+    } else {
+        res.send('No Matching Ticket Id... :(');
+    }
+});
+
+app.post('/api/tickets/:ticketId/undone', async (req, res) => {
+    let ticketJson = await fs.readFile('./data.json');
+    let ticketsArray = JSON.parse(ticketJson);
+    const doneTicketIndex = ticketsArray.findIndex(ticket => ticket.id === req.params.ticketId);
+    if (doneTicketIndex >= 0) {
+        ticketsArray[doneTicketIndex].done = false;
+        ticketJson = JSON.stringify(ticketsArray, null, 2);
+        await fs.writeFile('./data.json', ticketJson);
+        res.send(ticketsArray[doneTicketIndex]);
+    } else {
+        res.send('No Matching Ticket Id... :(');
+    }
+});
+
+
 
 module.exports = app;
