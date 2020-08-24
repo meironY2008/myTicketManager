5a548dcf037d7e6da417c7c2719506925c25006a
meironY2008 <67857621+meironY2008@users.noreply.github.com>
2020-08-24 15:53:35 +0300

    first test

--- server/app.js
+++ server/app.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const fs = require('fs').promises;
+const url = require('url');
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get(`/api/tickets/`, async (req, res) => {
+    const ticketJson = await fs.readFile('./data.json');
+    const ticketArray = JSON.parse(ticketJson);
+    const searchText = url.parse(req.url ,true).query.searchText;
+    if (searchText) {
+        const filterTickectsArray = ticketArray.filter(ticket => ticket.title.toLowerCase().includes(searchText.toLowerCase()))
+        res.send(filterTickectsArray);
+    }
+    else {
+        res.send(ticketArray);
+    }
+});
 
 module.exports = app;
