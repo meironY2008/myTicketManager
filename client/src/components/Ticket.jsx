e38432856fa4c3914d1458887e2e8e94bdc8f8ed
meironY2008 <67857621+meironY2008@users.noreply.github.com>
2020-08-25 17:53:26 +0300

    full date

--- client/src/components/Ticket.jsx
+++ client/src/components/Ticket.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from 'react';
 //import './App.css';
 
+
+const showFullDate = (ms) => {
+    const createdAt = new Date(ms);
+    return `${createdAt.getDate()}/${createdAt.getMonth() + 1}/${createdAt.getFullYear()} 
+    ${createdAt.getHours()}:${createdAt.getMinutes()}:${createdAt.getSeconds()}`;
+}
+
 function Ticket({ ticket, handleClick }) {
 
     const showLabel = (ticket) => {
@@ -12,7 +19,7 @@ function Ticket({ ticket, handleClick }) {
             <button className='hideTicketButton' onClick={()=>handleClick(ticket.id)}>HIDE</button>
             id: {ticket.id}, <br />
             title: {ticket.title},<br />
-            creation: { ticket.creationTime}<br />
+            creation: {showFullDate(ticket.creationTime)}<br />
             mail: {ticket.userEmail}<br />
             content: {ticket.content} <br />
             {ticket.labels && <div> labels: {showLabel(ticket)}</div>}
