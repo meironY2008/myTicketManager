d17cace67febd2e25e2b23d1e325c2eabd0bf2b5
meironY2008 <67857621+meironY2008@users.noreply.github.com>
2020-08-26 10:24:56 +0300

    srart style

--- client/src/components/Ticket.jsx
+++ client/src/components/Ticket.jsx
@@ -1,31 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 //import './App.css';
 
-
-const showFullDate = (ms) => {
-    const createdAt = new Date(ms);
-    return `${createdAt.getDate()}/${createdAt.getMonth() + 1}/${createdAt.getFullYear()} 
-    ${createdAt.getHours()}:${createdAt.getMinutes()}:${createdAt.getSeconds()}`;
-}
-
 function Ticket({ ticket, handleClick }) {
+  const [showMoreFlag, setShowMoreFlag] = useState(false);
 
-    const showLabel = (ticket) => {
-        return ticket.labels.map(label => (<div className="label" key={label}>{label}</div>))
-    }
+  const showFullDate = (ms) => {
+    const createdAt = new Date(ms);
+    return `${createdAt.getDate()}/${
+      createdAt.getMonth() + 1
+    }/${createdAt.getFullYear()} 
+        ${createdAt.getHours()}:${createdAt.getMinutes()}:${createdAt.getSeconds()}`;
+  };
 
-    return (
-        <div className="ticket">
-            <button className='hideTicketButton' onClick={()=>handleClick(ticket.id)}>HIDE</button>
-            id: {ticket.id}, <br />
-            title: {ticket.title},<br />
-            creation: {showFullDate(ticket.creationTime)}<br />
-            mail: {ticket.userEmail}<br />
-            content: {ticket.content} <br />
-            {ticket.labels && <div> labels: {showLabel(ticket)}</div>}
-        <br />
+  const showLabel = (ticket) => {
+    return ticket.labels.map((label) => (
+      <div className="label" key={label}>
+        {label}
+      </div>
+    ));
+  };
+
+  const showMore = () => {
+    setShowMoreFlag(!showMoreFlag);
+  };
+
+  return (
+    <div className="ticket">
+      <button
+        className="hideTicketButton"
+        onClick={() => handleClick(ticket.id)}
+      >
+        HIDE
+      </button>
+      id: {ticket.id}, <br />
+      <h1>{ticket.title}</h1>
+      <br />
+      creation: {showFullDate(ticket.creationTime)}
+      <br />
+      mail: {ticket.userEmail}
+      <br />
+      content:{" "}
+      {showMoreFlag ? (
+        <div>
+          {ticket.content}
+          <div className="show-more" onClick={() => showMore()}>
+            show less
+          </div>{" "}
         </div>
-    );
+      ) : (
+        <div>
+          <div> {ticket.content.substring(0, 100)} </div>{" "}
+          <div className="show-more" onClick={() => showMore()}>
+            show more...
+          </div>
+        </div>
+      )}
+      {ticket.labels && <div> labels: {showLabel(ticket)}</div>}
+      <br />
+    </div>
+  );
 }
 
 export default Ticket;
